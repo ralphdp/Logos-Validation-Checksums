b7311f2303f9549093e3176c4ea70141dcfeb761
Rafael De Paz <15041603+ralphdp@users.noreply.github.com>
2026-04-20 15:41:14 -0400

    chore: unify all checksum arrays into official json

--- manifest.ts
+++ manifest.ts
@@ -43,6 +43,11 @@ export const MANIFEST = {
             "hash": "a524d5d8fbe6ac17ed495c6253f3c3851c67a2ad3175fade8f7b242b66f8864d"
         },
         {
+            "id": "cosmological-master-equation-paper",
+            "label": "COSMOLOGICAL MASTER EQUATION PAPER",
+            "hash": "a1b54ddabb83b76b3fdf28f956fe4f3c97fb55461408e7a3a8d197c73b68948e"
+        },
+        {
             "id": "cosmological-parsimony-paper",
             "label": "COSMOLOGICAL PARSIMONY PAPER",
             "hash": "0689b68b9c6e68e7e35517989a3d563811a5de713a1660cde640c0484aabe4a7"
@@ -73,6 +78,11 @@ export const MANIFEST = {
             "hash": "bd7b8038721ca63c2ea1ec2a89478d86729b698a54de545f0273e70fc488f6d9"
         },
         {
+            "id": "generational-lineage-os-paper",
+            "label": "GENERATIONAL LINEAGE OS PAPER",
+            "hash": "89396fe9f4f473a43a83f9238bf5cc979c04562a4e6d4965a31292aad7182824"
+        },
+        {
             "id": "grace-protocol-dual-track-paper",
             "label": "GRACE PROTOCOL DUAL TRACK PAPER",
             "hash": "632200b6df1193dc1107d328e229a8f7aabd302af9f57e49efd25dcbfb65ce5d"
@@ -98,6 +108,11 @@ export const MANIFEST = {
             "hash": "eee76436e459fec15a651a7964188211887a917a7ff04ad5d53b56d3d6fdf8f3"
         },
         {
+            "id": "isomorphic-hardware-tethers-paper",
+            "label": "ISOMORPHIC HARDWARE TETHERS PAPER",
+            "hash": "603a239db3cdaed7ee7f3f525116c558cea87029174a3ef4fe87e4ff97ddd65e"
+        },
+        {
             "id": "multitime-transport-paper",
             "label": "MULTITIME TRANSPORT PAPER",
             "hash": "39590159cd2ecf298b9208f382b96f76a79ed8eec3e162e25139abd37761a461"
@@ -111,6 +126,11 @@ export const MANIFEST = {
             "id": "nile-protocol-distributed-grid-paper",
             "label": "NILE PROTOCOL DISTRIBUTED GRID PAPER",
             "hash": "27fbfb2fe9bd7020bee03b1d36f34683627a5de74190018b94654cc2ad93ccf4"
+        },
+        {
+            "id": "o1-validation-schema-caching-paper",
+            "label": "O1 VALIDATION SCHEMA CACHING PAPER",
+            "hash": "28b4771c06c7cc5e96a5104937fd2a6fdb135c7619d75512a46406208c78d261"
         },
         {
             "id": "p-vs-np-paper",
@@ -156,6 +176,11 @@ export const MANIFEST = {
             "id": "theodicy-paper",
             "label": "THEODICY PAPER",
             "hash": "b9723ca1f32c5e27c5783f78c83a06bfc71094de33f181d891878f51f79af0a4"
+        },
+        {
+            "id": "thermodynamic-morality-paper",
+            "label": "THERMODYNAMIC MORALITY PAPER",
+            "hash": "c688e84f9c2c12114be8cf7daf1b6c7985e3871f6e2221906566add9e529aa09"
         },
         {
             "id": "unitary-singularity-paper",
